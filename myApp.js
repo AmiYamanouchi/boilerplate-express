34f5151d0a7b123e9f8149c664b0c33a52da8f29
AmiYamanouchi <joe.b11s35@gmail.com>
2021-05-26 16:12:54 -0700

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ var app = express();
 
 // #3
   app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/views/index.html");
   });
 
 
