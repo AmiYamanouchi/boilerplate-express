bf6e3f897b9a1d16f788b54b6160dda94d67c340
AmiYamanouchi <joe.b11s35@gmail.com>
2021-05-27 11:12:05 -0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+
 // #1
 // console.log("Hello World")
 
@@ -10,13 +11,22 @@ var app = express();
 //   });
 
 // #3
-  app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-  });
+//   app.get("/", function(req, res) {
+//     res.sendFile(absolutePath);
+//   });
 
 // #4
-const assetPath = path.join(__dirname, "public");
-app.use(express.static(assetPath));
+// const assetPath = path.join(__dirname, "public");
+// app.use(express.static(assetPath));
+
+var absolutePath = __dirname + "/views/index.html"
+console.log(absolutePath);
+
+
+app.get('/', (req, res, next) => {
+  // res.send("Hello Express")
+  res.sendFile(absolutePath)
+})
 
 
 
